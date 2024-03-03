79bc75efa31ace7efb44d68d787a8fbc92a84dfd
yudai64 <kemurin.yudai0723@gmail.com>
2024-03-03 11:33:25 +0900

    add: eslintとprettier追加

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+    "prettier"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions":  {
+    "project": "./tsconfig.json"
+  },
+  "plugins": [
+    "import",
+    "@typescript-eslint"
+  ],
+  "root": true,
+  "rules": {
+    "import/order": [
+      "error",
+      {
+        "groups": ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+        alphabetize: {
+          order: "asc",
+        },
+      }
+    ]
+  }
+}  
