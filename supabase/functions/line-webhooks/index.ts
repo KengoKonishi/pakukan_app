89dda7417ec9121f4e6371bd93c643fa872c1b63
yudai64 <kemurin.yudai0723@gmail.com>
2024-04-23 19:13:44 +0900

    wip: シフト登録機能

--- supabase/functions/line-webhooks/index.ts
+++ supabase/functions/line-webhooks/index.ts
@@ -7,6 +7,9 @@ const LINE_PUSH_MESSAGE_URL = 'https://api.line.me/v2/bot/message/push'
 const CLEANER_REGISTRATION_FORM_BASE_URL =
   'https://docs.google.com/forms/d/e/1FAIpQLSfSBlpF_8oKHYkn5VNQsIb4EgYfA0ivi3f4I8LS9sjdJVa5rA/viewform?usp=pp_url&entry.455572547='
 
+const GOOGLE_CALENDAR_APP_URL =
+  'https://script.google.com/macros/s/AKfycbxLwMepLAnHWX9RlOa4kR1RyIVBF8u-tat6BxcPkSsK6s-jKJoXMgsPuEc5iLP28l0y/exec'
+
 type CarouselContainerContent = {
   type: 'bubble'
   body: {
@@ -278,9 +281,6 @@ const processEvent = async (event) => {
     event.type === 'postback' &&
     event.postback.data.startsWith('action=createCreanSchedule')
   ) {
-    // TODO
-    return
-
     const postbackData = new URLSearchParams(event.postback.data)
     const cleaningScheduleId = postbackData.get('id')
 
@@ -297,12 +297,19 @@ const processEvent = async (event) => {
       return
     }
 
-    const { count: updateCount, error } = await supabase
+    const {
+      count: updateCount,
+      data: updateCleaningScheduleData,
+      error,
+    } = await supabase
       .from('cleaning_schedules')
       .update({ cleaner_id: cleaner.id }, { count: 'exact' })
       .eq('id', cleaningScheduleId)
       // NOTE: シフトの重複登録を避けるために、更新条件として清掃員IDがNULLであることも指定しておく
       .is('cleaner_id', null)
+      .select(
+        'id, start_datetime, end_datetime, guest_houses (name), cleaners (name, email)',
+      )
 
     if (error) {
       console.error(error)
@@ -337,16 +344,34 @@ const processEvent = async (event) => {
     }
 
     // シフト登録できた場合
-    const { data: cleaningSchedule } = await supabase
-      .from('cleaning_schedules')
-      .select('id, start_datetime, end_datetime, guest_houses (name)')
-      .limit(1)
-      .single()
-      .eq('id', cleaningScheduleId)
+    const updateCleaningSchedule = updateCleaningScheduleData[0]
+    console.log(updateCleaningSchedule)
+    const guestHouse = updateCleaningSchedule.guest_houses.name
 
-    const startDatetime = new Date(cleaningSchedule.start_datetime).toLocaleString()
-    const endDatetime = new Date(cleaningSchedule.end_datetime).toLocaleString()
-    const scheduleInfo = `宿泊施設: ${cleaningSchedule.guest_houses.name}\n\n開始日: ${startDatetime}\n\n終了日: ${endDatetime}`
+    // Googleカレンダーに同期
+    try {
+      const body = {
+        action: 'createCleaningEvent',
+        summary: `${updateCleaningSchedule.cleaners.name}`,
+        startDateISOString: new Date(updateCleaningSchedule.start_datetime).toISOString(), // Googleカレンダー登録用
+        endDateISOString: new Date(updateCleaningSchedule.end_datetime).toISOString(), // Googleカレンダー登録用
+        guestHouse,
+        attendeesEmail: updateCleaningSchedule.cleaners.email,
+      }
+      console.log(body)
+      await fetch(GOOGLE_CALENDAR_APP_URL, {
+        method: 'POST',
+        body: JSON.stringify(body),
+      })
+    } catch (e) {
+      console.error('Googleカレンダーへの同期エラー')
+      console.error(e)
+      // NOTE: 同期に失敗したがシフト登録はできているため処理は継続する
+    }
+
+    const startDatetime = new Date(updateCleaningSchedule.start_datetime).toLocaleString() // LINE表示用
+    const endDatetime = new Date(updateCleaningSchedule.end_datetime).toLocaleString() // LINE表示用
+    const scheduleInfo = `宿泊施設: ${guestHouse}\n\n開始日: ${startDatetime}\n\n終了日: ${endDatetime}`
 
     const replyMessages = [
       {
