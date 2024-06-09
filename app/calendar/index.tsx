2c07a52a09cd7f97f16511ea7899b26118c197fc
yudai64 <kemurin.yudai0723@gmail.com>
2024-06-09 09:25:52 +0900

    Fix: カレンダーのヘッダー色修正

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -169,7 +169,7 @@ const AdminCalendar = () => {
             onChange={onChangeGuestHouseCheckBox}
           />
         </div>
-        <div className='w-4/6 overflow-y-auto bg-white'>
+        <div className='w-4/6 overflow-y-auto'>
           <Calendar
             events={schedules}
             selectable={true}
